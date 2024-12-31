b26b502a0be2aee4775bac0476623e124cf284ce
Benzi Holler <benzi.holler@freightverify.com>
2024-12-30 20:06:16 -0700

    update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <p className="footer-text">Benzi Holler &#xA9; 2023, Tucson, AZ</p>
+      <p className="footer-text">Benzi Holler &#xA9; 2024, Tucson, AZ</p>
     </div>
   );
 }
